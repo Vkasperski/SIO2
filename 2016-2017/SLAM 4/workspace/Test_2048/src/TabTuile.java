0537489ebe2a8051b0c60cc98359c19f6d050ffc
Vkasperski <victor.kasperski@laposte.net>
2016-10-25 22:11:04 +0200

    2048 class TaTuile

--- 2016-2017/SLAM 4/workspace/Test_2048/src/TabTuile.java
+++ 2016-2017/SLAM 4/workspace/Test_2048/src/TabTuile.java
@@ -1,3 +1,30 @@
-public class TabTuile {
-
+public class TabTuile 
+{
+	private
+		static int dimention;
+		int TabJeu[][];
+		
+	public TabTuile(int dim)
+	{
+		try
+		{
+			dimention = dim;
+			for(int i=0; i<dimention;i++)
+			{
+				for(int j=0; j<dimention;i++)
+				{
+					TabJeu[i][j]=0;
+				}
+			}
+		}
+		catch(Exception MauvaiseDimention)
+		{
+			System.out.println("Erreur la dimention de votre 2048 doit être positif non nul");
+		}
+	}
+	
+	public TabTuile popTuiles(TabTuile lol)
+	{
+		return lol;
+	}
 }
