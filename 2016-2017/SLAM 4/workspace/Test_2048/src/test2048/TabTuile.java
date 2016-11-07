422f50140c5cf87079fc78c9fb0c75296f453ac1
Vkasperski <victor.kasperski@laposte.net>
2016-11-07 08:55:03 +0100

    finition 2048

--- 2016-2017/SLAM 4/workspace/Test_2048/src/test2048/TabTuile.java
+++ 2016-2017/SLAM 4/workspace/Test_2048/src/test2048/TabTuile.java
@@ -1,6 +1,8 @@
+// KASPERSKI VICTOR : TEST 2048
+// 07/11/2016
+
 package test2048;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class TabTuile 
 {
@@ -368,78 +370,19 @@ public class TabTuile
 		return false;
 	}
 	
-	public static void Partie()
+	public boolean is2048()
 	{
-		Scanner dim = new Scanner(System.in);
-		Scanner choix = new Scanner(System.in);
-		int dimention;
-		char chx = '1';
-		do
+		for(int i=0; i<dimention-1;i++)
 		{
-			System.out.print("Dimention du 2048 : ");
-			try
+			for(int j=0;j<dimention-1;j++)
 			{
-				dimention = dim.nextInt();
-			}
-			catch(Exception e)
-			{
-				System.out.println("Dimention incorect dimention 4 cases sur 4 utilisé par défaut!!!");
-				dimention = 4;
-			}
-			TabTuile Game = new TabTuile(dimention);
-			TabTuile Save = new TabTuile(dimention);
-			Save.setTab(Game);
-				do
+				if(TabJeu[i][j]==2048)
 				{
-					if(chx=='1'||chx=='2'||chx=='g'||chx=='G'||chx=='d'||chx=='D'||chx=='b'||chx=='B'||chx=='h'||chx=='H')
-					{
-						if(Game.CheckMove(Save))
-						{
-							Game.popTuiles();
-							Game.popTuiles();
-						}
-					}
-					Game.affichTab();
-					Save.setTab(Game);
-					if(Game.MovePossible())
-					{
-						System.out.println(" 'g'->Gauche \n 'd'->Droite \n 'b'->Bas \n 'h'->Haut");
-						System.out.println(" '1'->reset \n '2'->Nouvelle Partie \n '3'->Quitter \n" );
-						chx = choix.next().charAt(0);
-						switch (chx)
-						{
-							case '1' :
-								Game.reset();
-								break;
-							case '2' :
-								System.out.println("\n\n\n\n");
-								break;
-							case '3' :
-								break;
-							case 'g' | 'G' :
-								Game.MoveLeft();
-								break;
-							case 'd' | 'D' :
-								Game.MoveRight();
-								break;
-							case 'h' | 'H' :
-								Game.MoveUp();
-								break;
-							case 'b' | 'B' :
-								Game.MoveDown();
-								break;
-							default :
-								System.out.println("\n Choix non disponible");
-								break;
-						}
-					}
-					else
-					{
-						System.out.println("Plus de mouvelent disponible");
-					}
-				}while(chx!='2' && chx !='3');
-		}while(chx!='3');
-		dim.close();
-		choix.close();
+					System.out.println("Vous avez gagner");
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
