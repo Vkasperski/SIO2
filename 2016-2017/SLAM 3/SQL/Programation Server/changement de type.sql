2005bc92d629643610720ae6898d6338d32553fc
Vkasperski <victor.kasperski@laposte.net>
2016-11-24 16:58:02 +0100

    SLAM 3

--- 2016-2017/SLAM 3/SQL/Programation Server/changement de type.sql
+++ 2016-2017/SLAM 3/SQL/Programation Server/changement de type.sql
@@ -1,24 +1,24 @@
+delimiter @
 DROP PROCEDURE IF EXISTS precotion;
 
 CREATE PROCEDURE precotion()
 	BEGIN
 		CREATE TABLE produit_save AS SELECT* FROM produit;
+		CREATE TABLE ligne_commande_save AS SELECT* FROM ligne_commande;
 
+		ALTER TABLE ligne_commande_save ENGINE = InnoDB;	
 		ALTER TABLE produit_save ENGINE = InnoDB;
+
 		ALTER TABLE produit_save
 		ADD CONSTRAINT pk_ref_save PRIMARY KEY (reference);
-		
-		
-		CREATE TABLE ligne_commande_save AS SELECT* FROM ligne_commande;
-		
-		ALTER TABLE ligne_commande_save ENGINE = InnoDB;		
+			
 		ALTER TABLE ligne_commande_save 
 		ADD CONSTRAINT pk_lco_save PRIMARY KEY (numero,reference),
 		ADD CONSTRAINT fk_num_save FOREIGN KEY (numero) REFERENCES commande (numero),
 		ADD CONSTRAINT fk_ref_save FOREIGN KEY (reference) REFERENCES produit_save(reference);		
 	END;
 @
-
+delimiter ;
 
 
 CREATE PROCEDURE ModifReference(reference int(5))
@@ -39,6 +39,6 @@ CREATE PROCEDURE ModifReference(reference int(5))
 
 		ALTER TABLE ligne_commande_save DROP FOREIGN KEY fk_ref_save;
 		ALTER TABLE produit_save DROP PRIMARY KEY pk_ref_save;
-		
+
 
 	END;
